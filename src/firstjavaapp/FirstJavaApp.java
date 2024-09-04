f6027c90f881b47122f65d42e894a0cfd73dd121
Joycie2523 <geocallocamillejoyce72@gmail.com>
2024-09-04 12:19:54 +0800

    Adding Account

--- src/firstjavaapp/FirstJavaApp.java
+++ src/firstjavaapp/FirstJavaApp.java
@@ -5,16 +5,16 @@ import java.util.Scanner;
 public class FirstJavaApp {
 
     public static void main(String[] args) {
-        //grades grd = new grades();
-        //grd.getGrade();
+      /*  grades grd = new grades();
+        grd.getGrade();
         
-        //Product pr = new Product();
-        //pr.addProduct(10011, "Soap", 20, 10);
-        //pr.viewProduct();
+        Product pr = new Product();
+        pr.addProduct(10011, "Soap", 20, 10);
+        pr.viewProduct();
         
-        //Product prl = new Product();
-        //prl.addProduct(10012, "Cards", 50, 0);
-        //prl.viewProduct();
+        Product prl = new Product();
+        prl.addProduct(10012, "Cards", 50, 0);
+        prl.viewProduct();
         
         
         Scanner sc = new Scanner(System.in);
@@ -56,4 +56,86 @@ public class FirstJavaApp {
         
     }
     
+}*/
+
+        Scanner sc = new Scanner(System.in);
+        Account[] ac = new Account[100];
+        
+        int nump, i;
+        System.out.print("Enter no. of Accounts: ");
+        nump = sc.nextInt();
+        
+        for(i = 0; i < nump; i++) {
+            System.out.println("Enter Details of Account " + (i + 1) + ": ");
+            
+            int id;
+            boolean sameID;
+            do {
+                sameID = false;    
+                System.out.print("Id: ");
+                id = sc.nextInt();
+                int x;
+                for(x = 0; x < i; x++) {
+                    if(ac[x].pid == id) {
+                        System.out.println("ID already exists!");
+                        sameID = true;
+                        break;
+                    }
+                }
+            } while(sameID);
+            
+            String fn, ln, eml, un, pw;
+            boolean sameUsername, sameEmail;
+            
+            System.out.print("Firstname: ");
+            fn = sc.next();
+            
+            System.out.print("Lastname: ");
+            ln = sc.next();
+            
+            do {
+                sameEmail = false;
+                System.out.print("Email: ");
+                eml = sc.next();
+                int x;
+                for(x = 0; x < i; x++) {
+                    if(ac[x].email.equals(eml)) {
+                        System.out.println("Email already exists!");
+                        sameEmail = true;
+                        break;
+                    }
+                }
+            } while(sameEmail);
+            
+            do {
+                sameUsername = false;
+                System.out.print("Username: ");
+                un = sc.next();
+                int x;
+                for(x = 0; x < i; x++) {
+                    if(ac[x].username.equals(un)) {
+                        System.out.println("Username already exists!");
+                        sameUsername = true;
+                        break;
+                    }
+                }
+            } while(sameUsername);
+        do {
+                System.out.print("Password: ");
+                pw = sc.next();
+                if (!Account.isPasswordValid(pw)) {
+                    System.out.println("Invalid Password, Please Enter Again: ");
+                }
+        }while (!Account.isPasswordValid(pw));
+            
+            ac[i] = new Account();
+            ac[i].addAccount(id, fn, ln, eml, un, pw);
+        }
+        
+        for(i = 0; i < nump; i++) {
+        ac[i].viewAccount();   
+
+        }
+
+    } 
 }
