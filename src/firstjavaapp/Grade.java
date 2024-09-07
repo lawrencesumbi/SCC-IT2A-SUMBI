ecee6d22ace51beb0654881d15046f30fe87d35c
lawrencesumbi <guiansumbi@gmail.com>
2024-09-07 18:44:25 +0800

    Revision of classes

--- src/firstjavaapp/Grade.java
+++ src/firstjavaapp/Grade.java
@@ -2,7 +2,7 @@ package firstjavaapp;
 
 import java.util.Scanner;
 
-public class grades {
+public class Grade {
     
     public void getGrade(){
     
--- src/firstjavaapp/Product.java
+++ src/firstjavaapp/Product.java
@@ -1,28 +1,43 @@
 package firstjavaapp;
 
+import java.util.Scanner;
+
 public class Product {
-    
-    String pname;
-    int pid, stocks, sold;
-    double price;
-    
-    public void addProduct(int id, String name, double prc, int stk, int sld){
-        this.pid = id;
-        this.pname = name;
-        this.price = prc;
-        this.stocks = stk;
-        this.sold = sld;
 
-    }
-    
-    public void viewProduct(){
+    Scanner sc = new Scanner(System.in);
+    Products[] pr = new Products[100];
         
-        double profit = this.price * this.sold;
-        String status = (this.stocks < 1) ? "Out-of-Stock" : "Available";
-        double tep = this.price * this.stocks;
-        
-        System.out.printf("%-10d %-10s % -10.2f %-10d %-10d %-10.2f %-10.2f %-10s \n",
-                this.pid, this.pname, this.price, this.stocks, this.sold, profit, tep, status);
+    int nump, i;
+    double ttp = 0, totalTEP = 0;
 
+    public void manageProducts() {
+        System.out.print("Enter number of products: ");
+        nump = sc.nextInt();
+        
+        for(i = 0; i < nump; i++) {
+            System.out.println("Enter details of Product " + (i+1) + ": ");
+            System.out.print("ID: ");
+            int id = sc.nextInt();
+            System.out.print("Name: ");
+            String name = sc.next();
+            System.out.print("Price: ");
+            double pri = sc.nextDouble();
+            System.out.print("Stocks: ");
+            int st = sc.nextInt();
+            System.out.print("Sold: ");
+            int sold = sc.nextInt();
+            
+            pr[i] = new Products();
+            pr[i].addProducts(id, name, pri, st, sold);
+        }
+
+        for(i = 0; i < nump; i++) {
+            pr[i].viewProducts();
+            ttp = ttp + (pr[i].price * pr[i].sold);
+            totalTEP = totalTEP + pr[i].price * pr[i].stocks;
+        }
+
+        System.out.println("Total Profit: " + ttp);
+        System.out.println("Total TEP: " + totalTEP);
     }
 }
